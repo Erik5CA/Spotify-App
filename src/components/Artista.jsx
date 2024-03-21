04184d11759b562b737708c275844df45d9066d2
Erik5CA <erik.castillo.anzures@gmail.com>
2024-03-21 16:41:27 -0600

    ancor para redireccionar

--- src/components/Artista.jsx
+++ src/components/Artista.jsx
@@ -1,11 +1,17 @@
 /* eslint-disable react/prop-types */
 
 export function Artista({ artista, seleccionarArtista }) {
-
-    return (
-        <li onClick={() => seleccionarArtista(artista.id)} className="artista">
-            <img src={artista.imagen} alt={`Imagen del artista : ${artista.nombre}`} />
-            <h3>{artista.nombre}</h3>
-        </li>
-    )
+  return (
+    <li onClick={() => seleccionarArtista(artista.id)}>
+      <a href="#albums" className="artista">
+        <img
+          src={artista.imagen}
+          alt={`Imagen del artista : ${artista.nombre}`}
+        />
+        <div className="nombre-artista">
+          <h3>{artista.nombre}</h3>
+        </div>
+      </a>
+    </li>
+  );
 }
