0bd7ebc2e946180f1344c7320fe1f2b4f9e4d238
Erik5CA <erik.castillo.anzures@gmail.com>
2024-03-21 16:46:37 -0600

    mas info para tracks y funcion para calcular duracion

--- src/services/tracks.js
+++ src/services/tracks.js
@@ -8,19 +8,33 @@ const options = {
   },
 };
 
+const toMinuteSecond = (milliseconds) => {
+  const minutes = Math.floor(milliseconds / 60000);
+  let seconds = ((milliseconds % 60000) / 1000).toFixed(0);
+  if (seconds.length < 2) {
+    seconds = `0${seconds}`;
+  }
+  return `${minutes}:${seconds}`;
+};
+
 export const searchTracks = async ({ albumId }) => {
   // console.log('searchAlbums')
   try {
     const urlTracks = `https://spotify-web2.p.rapidapi.com/album_tracks/?id=${albumId}&offset=0&limit=300`;
     const resonse = await fetch(urlTracks, options);
     const data = await resonse.json();
+    console.log(data);
     const tracks = data.data.album.tracks.items;
     const newTracks = tracks.map((track) => ({
       idTrack: track.track.uri.split(":")[2],
       numberTrack: track.track.trackNumber,
       nameTrack: track.track.name,
+      duration: toMinuteSecond(track.track.duration.totalMilliseconds),
+      playcount: track.track.playcount,
+      explicit: track.track.contentRating.label,
+      artists: track.track.artists.items?.map((artist) => artist.profile.name),
     }));
-    // console.log(newTracks);
+    console.log(newTracks);
     return newTracks;
   } catch (error) {
     throw new Error("Error al buscar los albumes.");
