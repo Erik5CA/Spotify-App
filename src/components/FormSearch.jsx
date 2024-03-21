241ceb3cce55f656747c1a00d9eb69563f6ac54f
Erik5CA <erik.castillo.anzures@gmail.com>
2024-03-21 16:42:03 -0600

    svg buscar y mensaje de error

--- src/components/FormSearch.jsx
+++ src/components/FormSearch.jsx
@@ -1,21 +1,39 @@
 /* eslint-disable react/prop-types */
 
-import '../styles/FormSearch.css'
+import "../styles/FormSearch.css";
 
-export function FormSearch({ handleSumit, handleChange, busqueda }) {
-    return (
-        <form className="form-search" onSubmit={handleSumit}>
-            <input
-                className='input-form-search'
-                onChange={handleChange}
-                value={busqueda}
-                name='query'
-                type='search'
-                placeholder='Busca un artista'
+export function FormSearch({ handleSumit, handleChange, busqueda, error }) {
+  return (
+    <form className="contenedor-form-search" onSubmit={handleSumit}>
+      <div className="form-search">
+        <input
+          className="input-form-search"
+          onChange={handleChange}
+          value={busqueda}
+          name="query"
+          type="search"
+          placeholder="Busca un artista"
+        />
+        <button className="button-form-search" type="submit">
+          <svg
+            width={15}
+            height={15}
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
             />
-            <button
-                className='button-form-search'
-                type='submit'  >Buscar</button>
-        </form>
-    )
+          </svg>
+          Buscar
+        </button>
+      </div>
+      {error && <p>{error}</p>}
+    </form>
+  );
 }
