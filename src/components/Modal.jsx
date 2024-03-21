81fe95a87b76bb21853821db536835d0de54308e
Erik5CA <erik.castillo.anzures@gmail.com>
2024-03-21 16:44:15 -0600

    svg de cierre y mas informacion en los tracks

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,12 +6,36 @@ export function Modal({ onClose, tracks, album }) {
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal" onClick={(e) => e.stopPropagation()}>
         <button className="modal-close" onClick={onClose}>
-          Cerrar
+          <svg
+            width={30}
+            height={30}
+            xmlns="http://www.w3.org/2000/svg"
+            // fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
         </button>
+        <h3 className="album-name">{album.nameAlbum}</h3>
         <img className="modal-imagen" src={album.imagenAlbum} />
         {tracks?.map((track) => (
           <div className="track" key={track.idTrack}>
-            <h3 className="modal-name-track">{track.nameTrack}</h3>
+            <div>
+              <h3 className="modal-name-track">{track.nameTrack}</h3>
+              <p>{track.artists.map((artist) => artist + " | ")}</p>
+            </div>
+            <div className="track-info">
+              <p>{track.duration}</p>
+              {track.explicit !== "NONE" && (
+                <p className="track-explicit">{track.explicit[0]}</p>
+              )}
+            </div>
           </div>
         ))}
       </div>
