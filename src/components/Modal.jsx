f514ada81ff07b8fb59275c9843071ed308c4947
Erik5CA <erik.castillo.anzures@gmail.com>
2024-03-20 18:03:17 -0600

    recolacion imagen

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,21 +1,20 @@
 /* eslint-disable react/prop-types */
-import '../styles/Modal.css'
+import "../styles/Modal.css";
 
 export function Modal({ onClose, tracks, album }) {
-
-    return (
-        <div className="modal-overlay" onClick={onClose}>
-            <div className="modal" onClick={(e) => e.stopPropagation()}>
-                <button className="modal-close" onClick={onClose}>Cerrar</button>
-                {
-                    tracks?.map((track) => (
-                        <div className="track" key={track.idTrack}>
-                            <img className="modal-imagen" src={album.imagenAlbum} />
-                            <h3 className="modal-name-track">{track.numberTrack} - {track.nameTrack}</h3>
-                        </div>
-                    ))
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal" onClick={(e) => e.stopPropagation()}>
+        <button className="modal-close" onClick={onClose}>
+          Cerrar
+        </button>
+        <img className="modal-imagen" src={album.imagenAlbum} />
+        {tracks?.map((track) => (
+          <div className="track" key={track.idTrack}>
+            <h3 className="modal-name-track">{track.nameTrack}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
