24090b5984f485ad3d17a3ab322d0494504f8234
Erik5CA <erik.castillo.anzures@gmail.com>
2024-04-22 11:54:42 -0600

    added component loader

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/prop-types */
 import "../styles/Modal.css";
+import Loading from "./Loading";
 
-export function Modal({ onClose, tracks, album }) {
+export function Modal({ onClose, tracks, album, loading }) {
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal" onClick={(e) => e.stopPropagation()}>
@@ -23,21 +24,27 @@ export function Modal({ onClose, tracks, album }) {
           </svg>
         </button>
         <h3 className="album-name">{album.nameAlbum}</h3>
-        <img className="modal-imagen" src={album.imagenAlbum} />
-        {tracks?.map((track) => (
-          <div className="track" key={track.idTrack}>
-            <div>
-              <h3 className="modal-name-track">{track.nameTrack}</h3>
-              <p>{track.artists.map((artist) => artist + " | ")}</p>
-            </div>
-            <div className="track-info">
-              <p>{track.duration}</p>
-              {track.explicit !== "NONE" && (
-                <p className="track-explicit">{track.explicit[0]}</p>
-              )}
-            </div>
-          </div>
-        ))}
+        <img className="modal-imagen" src={album.imagenAlbum} loading="lazy" />
+        {loading ? (
+          <Loading />
+        ) : (
+          <>
+            {tracks?.map((track) => (
+              <div className="track" key={track.idTrack}>
+                <div>
+                  <h3 className="modal-name-track">{track.nameTrack}</h3>
+                  <p>{track.artists.map((artist) => artist + " | ")}</p>
+                </div>
+                <div className="track-info">
+                  <p>{track.duration}</p>
+                  {track.explicit !== "NONE" && (
+                    <p className="track-explicit">{track.explicit[0]}</p>
+                  )}
+                </div>
+              </div>
+            ))}
+          </>
+        )}
       </div>
     </div>
   );
