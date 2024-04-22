9a6e79c02e5963d3c85729c3bcaa5ba5faef99b8
Erik5CA <erik.castillo.anzures@gmail.com>
2024-04-22 11:54:09 -0600

    component loader

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import '../styles/Loader.css'
+export function Loading() {
+  return (
+    <div className="loader">
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+    </div>
+  )
+}
+
+export default Loading
